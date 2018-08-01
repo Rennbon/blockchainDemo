76b64f37962d39baaad6a8e6b9c6ad5dae378c59
rennbon <>
2018-08-01 14:52:32 +0800

    btcDaemon push test

--- wallets/wallet_0_btc_daemon_test.go
+++ wallets/wallet_0_btc_daemon_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 	"time"
 	"log"
+	"sync"
 )
 
 
 ///////////////////////////////////pool-test////////////////////////////////////////////////
+//验证填充块高
 func TestFillBlockHeight(t *testing.T) {
 	hash, _ := chainhash.NewHashFromStr("88af33f7e455751ae130746f7a7bd6538fc8b791aa67692dba33ab450ada9c92")
 	_txc := &txexcuting{
@@ -21,10 +23,12 @@ func TestFillBlockHeight(t *testing.T) {
 	t.Log(_txc.targetH, _txc.blockH)
 }
 
+//验证实例化btcDaemon
 func TestNewBTCDaemon(t *testing.T) {
 	daemon := NewBTCDaemon(time.NewTicker(5*time.Second))
 	t.Log( daemon.blkHt)
 }
+//验证监听区块高度并重绘内存中缓存的全局唯一高度
 func TestMonitoringBtcBlockHeight(t *testing.T) {
 	daemon := NewBTCDaemon(time.NewTicker(1*time.Second))
 	height :=daemon.blkHt
@@ -43,4 +47,46 @@ func TestMonitoringBtcBlockHeight(t *testing.T) {
 				}
 		}
 	}
+}
+//测试push
+//
+//  1) 往btcDaemon中推入交易参数
+//  2) local池达到限高后会转移到global池，local计数的参数会直接进入exch通道消费
+func TestBtcDaemon_push(t *testing.T) {
+	daemon := NewBTCDaemon(time.NewTicker(5 * time.Second))
+	wg := new(sync.WaitGroup)
+	count :=30
+	wg.Add(count)
+	go func(){
+		for i:=float64(0);i<float64(count);i++{
+			time.Sleep(5*time.Second)
+			transfer, _ := btcCoin.FloatToCoinAmout(i)
+			fee, _ := btcCoin.StringToCoinAmout("0.0001")
+			txrchan :=make( chan *TxResult)
+			daemon.push("输入地址","输出地址",transfer,fee,txrchan)
+			wg.Done()
+		}
+	}()
+	//这里只打印localPoolCount个（写测试时订的是10）
+	go func (d *btcDaemon){
+		i:=0
+		for ch:=range d.exch{
+			i++
+			log.Println("第",i,"个",ch.addf,ch.transfer.String())
+		}
+	}(daemon)
+
+	go func(d *btcDaemon){
+		for {
+			select {
+				case <- d.tick.C:
+					//从0开始累加，到localPoolCount个后不在递增，转移至global缓存
+					log.Println("打印检测,local-",d.lpl.size,"-",d.lpl.deadline)
+				    //local未达到localPoolCount时，这里都为0 ，等d.lpl溢出（超过localPoolCount）后开始累加，无上限
+					log.Println("打印检测,global-",d.gpl.size)
+			}
+
+		}
+	}(daemon)
+	wg.Wait()
 }
